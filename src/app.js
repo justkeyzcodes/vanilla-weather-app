d15d07afccd7405ab1f3c7e981c2988d4d1fea62
justkeyzcodes <89028424+justkeyzcodes@users.noreply.github.com>
2021-10-28 19:58:25 -0400

    Updated JavaScript

--- src/app.js
+++ src/app.js
@@ -37,8 +37,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
-let city = "Nottingham";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Nottingham");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
