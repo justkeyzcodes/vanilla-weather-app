7767175131876c959f31850958e36158efd8608c
justkeyzcodes <89028424+justkeyzcodes@users.noreply.github.com>
2021-10-29 14:00:21 -0400

    Added image folder and new image

--- src/app.js
+++ src/app.js
@@ -49,7 +49,17 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (13 * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
 search("Nottingham");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
