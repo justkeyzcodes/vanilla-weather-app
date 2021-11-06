438dd004c2d5d057c1e63682d5534bed24af9dd9
justkeyzcodes <89028424+justkeyzcodes@users.noreply.github.com>
2021-11-06 11:19:33 -0400

    Removed Console.log

--- src/app.js
+++ src/app.js
@@ -59,14 +59,13 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "46fac47dd8b8fa26d1b6852218ad3dfe";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
